2b920ee1ff926db0adc89a753ce5ba8eef607d78
Aluno <kaua.moreno2005@gmail.com>
2023-04-25 10:35:45 -0300

    finalizando desafio5.sql

--- desafios/desafio5/desafio5.sql
+++ desafios/desafio5/desafio5.sql
@@ -46,7 +46,16 @@ LEFT JOIN Unidade_Curricular
 
 -------------------------------------------------------------------------
 -- 5. Quais os professores que não receberam nenhuma atribuição;
-
+SELECT
+	nome_professor AS 'Professor',
+	nome_unidade_Curricular
+FROM
+	AtribuiçãoAula	
+FULL JOIN Professor
+	ON AtribuiçãoAula.id_professor = Professor.id_professor
+LEFT JOIN Unidade_Curricular
+	ON AtribuiçãoAula.id_unid_Curricular = Unidade_Curricular.id_unid_Curricular
+WHERE nome_unidade_Curricular IS NULL
 
 -------------------------------------------------------------------------
 -- 6. Qual o total de curso para cada formação; CT, CAI e FIC;
@@ -67,10 +76,49 @@ LEFT JOIN Professor
 GROUP BY Turma.nome_turma 
 
 
+-------------------------------------------------------------------------
 -- 8. Quais os professores que pegaram mais de uma turma e quantas foram;
+SELECT
+	nome_professor,
+	COUNT(DISTINCT nome_turma) AS 'qtd. Turma'
+FROM
+	AtribuiçãoAula
+LEFT JOIN Professor
+	ON AtribuiçãoAula.id_professor = Professor.id_professor
+LEFT JOIN Turma
+	ON AtribuiçãoAula.id_turma = Turma.id_turma
+GROUP BY nome_professor
+HAVING COUNT(DISTINCT nome_turma) > 1;
+
+
+-------------------------------------------------------------------------
 -- 9. O total de turma registrada;
+SELECT
+COUNT(*) AS 'Total de Turmas'
+FROM
+	Turma
+
+
+-------------------------------------------------------------------------
 -- 10. Qual o total de turmas formadas;
+SELECT
+	COUNT(nome_turma) AS 'qtd de turmas'
+FROM
+	Turma
+
+
+-------------------------------------------------------------------------
 -- 11. Quantos alunos existem em cada turma;
+SELECT
+	nome_turma,
+	COUNT(primeiroNome) 'qtd. Aluno'
+FROM
+	ListaAluno
+LEFT JOIN Turma
+	ON ListaAluno.id_turma = Turma.id_turma
+LEFT JOIN Aluno
+	ON ListaAluno.id_aluno = Aluno.id_aluno
+GROUP BY nome_turma
 
 
 -------------------------------------------------------------------------
@@ -80,4 +128,29 @@ GROUP BY Turma.nome_turma
 
 -------------------------------------------------------------------------
 -- 13. Qual a turma que tem mais alunos;
+SELECT
+	nome_turma,
+	COUNT(primeiroNome) 'qtd. Aluno'
+FROM
+	ListaAluno
+LEFT JOIN Turma
+	ON ListaAluno.id_turma = Turma.id_turma
+LEFT JOIN Aluno
+	ON ListaAluno.id_aluno = Aluno.id_aluno
+GROUP BY nome_turma
+ORDER BY COUNT(primeiroNome) DESC
+
+
+-------------------------------------------------------------------------
 -- 14. Qual a turma que tem menos alunos.
+SELECT
+	nome_turma,
+	COUNT(primeiroNome) 'qtd. Aluno'
+FROM
+	ListaAluno
+LEFT JOIN Turma
+	ON ListaAluno.id_turma = Turma.id_turma
+LEFT JOIN Aluno
+	ON ListaAluno.id_aluno = Aluno.id_aluno
+GROUP BY nome_turma
+ORDER BY COUNT(primeiroNome) ASC
